296d10d02cc22e7228a281e50afe5bd4ebf3fafa
Seapol Shen <seapol.shen@infineon.com>
2021-08-02 13:17:44 +0800

    Add project files.

---
+++ MTKCurrentBoard_rev2.0_test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKCurrentBoard_rev2._0_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
